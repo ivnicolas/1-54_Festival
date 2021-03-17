8184f533f160fd7dbe8ce5d912863bdf8bf52643
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-03-17 16:37:55 -0400

    added comments for scrapper.rb

---
+++ lib/scrapper.rb
@@ -0,0 +1,11 @@
+class Scrapper 
+    #Scrape Artist
+        #scrapes artist at New York exhbit for name and url 
+    #Scrape Individual Artist 
+        #Here is where you collect gallery, bio, and about message 
+    
+
+
+    #**be mindful that artist name are displayed last name to first, so where in any of the lib classes ill you fix this so
+    #it will be displayed first name last name? 
+end 
