60275b95a6cb0d10a5bbe01a1dd743cac7dd7284
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-03-17 16:33:43 -0400

    added comments

---
+++ lib/cli.rb
@@ -0,0 +1,34 @@
+class CLI
+
+    #run method 
+        #puts welcome message with blurb about festival
+        #Ask user for selection
+        #Scrape Artist
+        #list artist in right number and numerically (list artist method)
+        #menue
+
+    #menue method
+        #Ask user to Choose Artist
+        #make sure entry is valid
+            #if not valid riase error and print error message
+            #if valid call artist_artist details method
+        #call closing method
+
+    #list artist method
+        #Instantiates though artist return a list of artist back in a numbered list and artist and listed alphabetically
+
+    #list artist_details method
+        #Scrape Individual Artist
+        # Puts intro message 
+        # Puts bio message
+        # Puts gallery info
+        # Puts paragraph about artist's work 
+
+    #closing method
+        #Ask user if they want to continue exploring artist or exit
+            #if they say continue run menue method
+            #if they say exit, exit application
+
+
+
+end 
