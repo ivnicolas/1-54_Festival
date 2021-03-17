cd9012c5b41c0cfc017325c281e3788de506118f
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-03-17 16:22:26 -0400

    added comments to artist.rb

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+    #add attr_accessors 
+
+    #collect all instances
+
+    #initialize with two arguments (name, url)
+    #push into collections
+
+    #method that prints out all instances 
+
+end
