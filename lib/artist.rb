4a0108f32d473e6f141e6e3ea70ba5c6198aeeb6
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-03-17 16:41:10 -0400

    added code to Artist class

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,23 @@
 class Artist
     #add attr_accessors 
+    attr_accessor :name, :url, :gallery, :bio, :about_art
 
     #collect all instances
 
-    #initialize with two arguments (name, url)
-    #push into collections
+    @@all = []
+
+    #initialize with two arguments (name, url) an push into @@all
+    def initialize(name,url)
+        @name = name
+        @url = url
+        @@all << self
+
+    end 
 
     #method that prints out all instances 
+
+    def self.all
+        @@all
+    end 
 
 end
