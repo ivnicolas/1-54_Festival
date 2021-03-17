7307c5de775ae437b7e6f2fc182d89b61616c521
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-03-17 16:19:37 -0400

    added requirement to environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require "open-uri"
+require "nokogiri"
+require "pry"
+
+require_relative "./lib/cli.rb"
+require_relative "./lib/scrapper.rb"
+require_relative "./lib/artist.rb"   
