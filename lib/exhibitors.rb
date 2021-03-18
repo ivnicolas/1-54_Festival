f84254e253f0508c499703f51afc56c45f31cfdf
Ikenna Nicolas <ikennanicolas@gmail.com>
2021-03-18 19:58:31 -0400

    created exhibitors class

---
+++ lib/exhibitors.rb
@@ -0,0 +1,15 @@
+class Exhibitors 
+    attr_accessor :name, :url, :artists
+
+    @@all = []
+
+    def initialize (name,url)
+        @name= name
+        @url= url
+        @@all << self
+    end 
+
+    def self.all 
+        @@all
+    end 
+end 
